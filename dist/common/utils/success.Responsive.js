a8049cad8632a29acb4de140e4d9b7aabc48ca04
magdyyoussef912-ops <magdyyoussef912@gmail.com>
2026-05-02 21:39:42 +0300

    Delete dist directory

--- dist/common/utils/success.Responsive.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.successResponse = void 0;
-const successResponse = ({ res, data, message = "Done", statusCode = 200 }) => {
-    return res.status(statusCode).json({ message, data });
-};
-exports.successResponse = successResponse;
